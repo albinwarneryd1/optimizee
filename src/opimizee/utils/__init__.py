1df1f75d35a3b667667779799b3c19f95844ede1
Albin Wärneryd <al3765ho-s@student.lu.se>
2026-02-10 23:20:54 +0100

    chore: export config constants

---
+++ src/opimizee/utils/__init__.py
@@ -0,0 +1,10 @@
+from .config import PROJECT_ROOT, DATA_RAW, DATA_PROCESSED, MODELS_DIR, REPORTS_DIR, DataSchema
+
+__all__ = [
+    "PROJECT_ROOT",
+    "DATA_RAW",
+    "DATA_PROCESSED",
+    "MODELS_DIR",
+    "REPORTS_DIR",
+    "DataSchema",
+]
